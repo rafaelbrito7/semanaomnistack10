1bbbeafd8d10f7d090ab3d48a394dd3956040fde
Rafael Souza Pereira de Brito <rafael.souzabrito7@gmail.com>
2020-01-15 20:50:54 -0300

    front end started

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+//Componente: Bloco isolado de HTML, CSS e JS, o qual não interfere no restante da aplicação
+//Propriedade: Informações que um componente PAI passa para o componente filho
+//Estado
+
+function App() {
+  function incrementCounter() {
+    let contador = 0;
+  }
+
+  return (
+    <>
+      <h1>Contador: 0</h1>
+      <button>Incrementar</button>
+    </>
+  );
+}
+
+export default App;
