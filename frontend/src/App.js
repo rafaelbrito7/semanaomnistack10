efb82d10ac142b5764edeefb4590d4156c34c82b
Rafael Souza Pereira de Brito <rafael.souzabrito7@gmail.com>
2020-01-16 16:49:59 -0300

    added css and components files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+import './global.css';
+import './App.css';
 //Componente: Bloco isolado de HTML, CSS e JS, o qual não interfere no restante da aplicação
 //Propriedade: Informações que um componente PAI passa para o componente filho
-//Estado
+//Estado: Informações mantidas pelo componente (Lembrar: Imutabilidade)
 
 function App() {
-  function incrementCounter() {
-    let contador = 0;
-  }
-
   return (
-    <>
-      <h1>Contador: 0</h1>
-      <button>Incrementar</button>
-    </>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input name="github_username" id="github_username" required></input>
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required></input>
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required></input>
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="longitude">longitude</label>
+              <input name="longitude" id="longitude" required></input>
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+      <main></main>
+    </div>
   );
 }
 
